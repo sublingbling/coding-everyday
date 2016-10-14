40d39f63c349e5865d30f5dd69f6db84c75e4bd8
up824 <gin.1up@gmail.com>
2016-10-14 01:45:44 -0700

    Update up.py

--- 10_14_2016_remove-nth-node-from-end-of-list/up.py
+++ 10_14_2016_remove-nth-node-from-end-of-list/up.py
@@ -62,3 +62,42 @@ class Solution(object):
         prev.next = prev.next.next
         
         return root.next
+
+### Actually I don't need a dummy head to remove the first node, I can just return head.next....
+### so change the code a little bit....
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+
+        # move "end" anyway 
+        end = head 
+        for _ in xrange(n):
+            end = end.next
+        
+        # if "end" reaches the end, remove the first node
+        if not end:
+            return head.next
+        
+        # well, it's not the end, I need a root now...
+        root = head
+        # and hack "end" a bit, to make "head" as a "prev"
+        end = end.next
+    
+        # here we go, to the end... 
+        while end:
+            head = head.next
+            end =  end.next
+        head.next = head.next.next
+        
+        return root

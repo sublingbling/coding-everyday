c44a5e761be48ec74d8db6c00b4f61f52562a2aa
tflchina <tflchina@gmail.com>
2016-10-13 22:07:10 -0700

    Update feng.cpp
    add non-recursive solution

--- 10_13_2016_merge_two_sorted_lists/feng.cpp
+++ 10_13_2016_merge_two_sorted_lists/feng.cpp
@@ -17,3 +17,39 @@ public:
         return l1;  
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    void sort(ListNode*& l1, ListNode*& l2){
+        if(l1->val > l2->val){ListNode* t= l1; l1=l2; l2=t;}
+    }
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        sort(l1,l2);
+        ListNode* head=l1;
+        ListNode* tail=l1;
+        l1 = l1->next;
+        
+        while(l1 && l2){
+            sort(l1,l2);
+            tail->next = l1;
+            tail = l1;
+            l1 = l1->next;
+        }
+        tail->next = l2;
+        return head;  
+    }
+};

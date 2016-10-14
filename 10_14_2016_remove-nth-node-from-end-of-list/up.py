171263ccbbe15f05afccd117c69165ecd8cafaa9
up824 <gin.1up@gmail.com>
2016-10-14 01:31:41 -0700

    Update up.py

--- 10_14_2016_remove-nth-node-from-end-of-list/up.py
+++ 10_14_2016_remove-nth-node-from-end-of-list/up.py
@@ -29,3 +29,36 @@ class Solution(object):
         prev.next = prev.next.next
         
         return root.next
+
+    
+    
+##### WAIT, why I need "end"? The "head" is not even used... 
+##### 2.0 - remove "end" and let "head" do it. And no need to check n==0 case...
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        
+        ## create root and prev. Prev will point to the target node when head reach the end
+        prev = root = ListNode(0)
+        root.next = head 
+        for _ in xrange(n):
+            head = head.next
+        
+        # delete the node prev.next
+        while head:
+            head = head.next
+            prev = prev.next
+        prev.next = prev.next.next
+        
+        return root.next

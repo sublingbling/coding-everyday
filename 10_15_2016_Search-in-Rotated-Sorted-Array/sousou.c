cd0db9fcf093c54f20846744322a5e4635ee0f1d
jackzwj1988 <jackzwj1988@gmail.com>
2016-10-15 16:37:32 -0700

    Create sousou.c

---
+++ 10_15_2016_Search-in-Rotated-Sorted-Array/sousou.c
@@ -0,0 +1,45 @@
+// bloody ugly one...
+
+int search(int* nums, int numsSize, int target) {
+    if (!nums || 0==numsSize) return -1;
+    if (1 == numsSize && target != nums[0]) return -1;
+    
+    if (target == nums[0]) return 0;
+    if (target == nums[numsSize/2]) return numsSize/2;
+    if (target == nums[numsSize-1]) return numsSize-1;
+    
+    if (nums[0] < nums[numsSize/2])
+    {
+        // if pivot is at right half
+        if (target>nums[0] && target<nums[numsSize/2])
+        {
+            // left half
+            return search(nums, numsSize/2, target);
+        }
+        else
+        {
+            // right half
+            int retVal = search(nums+numsSize/2, numsSize - numsSize/2, target);
+            if (retVal >=0) return numsSize/2 + retVal;
+            else return retVal;
+        }
+    }
+    else
+    {
+        // pivot is at left half
+        if (target>nums[numsSize/2] && target<nums[numsSize-1])
+        {
+            // right half
+            int retVal = search(nums+numsSize/2, numsSize - numsSize/2, target);
+            if (retVal >=0) return numsSize/2 + retVal;
+            else return retVal;
+        }
+        else
+        {
+            // right half
+            return search(nums, numsSize/2, target);
+        }
+    }
+
+    return -1;
+}

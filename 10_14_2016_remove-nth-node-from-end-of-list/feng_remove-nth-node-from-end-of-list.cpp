47fc27e026fec767236b4d79a1187956cdc0135d
tflchina <tflchina@gmail.com>
2016-10-14 21:24:37 -0700

    Create feng_remove-nth-node-from-end-of-list.cpp

---
+++ 10_14_2016_remove-nth-node-from-end-of-list/feng_remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast;
+        ListNode* slow = new ListNode(0); //use dummy head
+        slow->next = fast = head;
+        
+        for(int i=0;i<n;i++) fast=fast->next;
+        if(fast==NULL) return head->next; //if nth is first element
+        
+        while(fast){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};

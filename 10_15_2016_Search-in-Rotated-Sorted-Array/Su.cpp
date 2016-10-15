34ab7665ddead20b12582fbdc898df85cef29bd2
sublingbling <shopxiaohao@gmail.com>
2016-10-15 11:11:10 -0700

    Update Su.cpp

--- 10_15_2016_Search-in-Rotated-Sorted-Array/Su.cpp
+++ 10_15_2016_Search-in-Rotated-Sorted-Array/Su.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lastIndex = nums.size()-1;
+        int firstIndex = 0;
+        
+        while(target>nums[firstIndex] && nums[firstIndex]<nums[firstIndex+1]) firstIndex++;
+        if(target==nums[firstIndex]) return firstIndex;
+        
+        while(target<nums[lastIndex] && nums[lastIndex]>nums[lastIndex-1]) lastIndex--;
+        if(target==nums[lastIndex]) return lastIndex;
+        
+        return -1;
+    }
+};

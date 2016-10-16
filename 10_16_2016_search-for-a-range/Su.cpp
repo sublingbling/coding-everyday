dd24876d504df5190971de48b5ecf2791684f49d
sublingbling <shopxiaohao@gmail.com>
2016-10-16 16:12:48 -0700

    Create Su.cpp

---
+++ 10_16_2016_search-for-a-range/Su.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int L=0;
+        int R=nums.size()-1;
+        int M;
+        vector<int> result(2,-1);
+        while(L<=R){
+            M=(L+R)/2;
+            if(nums[M]==target) break;
+            if(nums[M]<target) L=M+1;
+            else R=M-1;
+        }
+        if(L<=R){
+            result[0]=M;
+            result[1]=M;
+            
+            for(L=M;nums[L-1]==nums[M] && L>0;) L--;
+            result[0]=L;
+            for(R=M;nums[R+1]==nums[M] && R<nums.size()-1;) R++;
+            result[1]=R;
+        }
+        return result;
+    }
+};

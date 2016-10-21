e7b0babec3d6d5e12ec658b25b9e1ff65f9dff10
jackzwj1988 <jackzwj1988@gmail.com>
2016-10-20 19:19:42 -0700

    Create sousou.c

---
+++ 10_20_2016_Maximum-Depth-of-Binary-Tree/sousou.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if (!root) return 0;
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+    return (leftDepth>rightDepth ? leftDepth : rightDepth) +1;
+}

302b3a919971a3a9bccd9240dcc64cca79f3b7de
up824 <gin.1up@gmail.com>
2016-10-22 13:51:25 +0800

    Create up.py

---
+++ 10_21_2016_Binary-Tree-Preorder-Traversal/up.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        re = []
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                re += [node.val]
+                stack += [node.right]
+                stack += [node.left]
+        return re

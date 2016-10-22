fd7d3d7dc92297244908f7c06c5c6a0a9d658a66
tflchina <tflchina@gmail.com>
2016-10-21 22:53:36 -0700

    Update fenglei.cpp

--- 10_21_2016_Binary-Tree-Preorder-Traversal/fenglei.cpp
+++ 10_21_2016_Binary-Tree-Preorder-Traversal/fenglei.cpp
@@ -21,3 +21,33 @@ public:
         return result;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> r;
+        stack<TreeNode*> s;
+        s.push(root);
+        TreeNode* t;
+        while(!s.empty()){
+            t = s.top();
+            s.pop();
+            if(t!=NULL){
+                r.push_back(t->val);
+                s.push(t->right);
+                s.push(t->left);
+            }
+        }
+        return r;
+    }
+};

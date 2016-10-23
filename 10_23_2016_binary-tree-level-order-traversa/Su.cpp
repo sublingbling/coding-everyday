2cd2309c0cae415287db56285ec097387089fdf1
sublingbling <shopxiaohao@gmail.com>
2016-10-23 12:15:11 -0700

    Update Su.cpp

--- 10_23_2016_binary-tree-level-order-traversa/Su.cpp
+++ 10_23_2016_binary-tree-level-order-traversa/Su.cpp
@@ -2,32 +2,20 @@
         vector<vector<int>> result;
         if(root==NULL) return result;
         vector<int> level;
-        queue<TreeNode*> cont1;
-        queue<TreeNode*> cont2;
-        cont1.push(root);
-        while(cont1.size() || cont2.size()){
-            while(cont1.size()){
-                root = cont1.front();
-                cont1.pop();
+        queue<TreeNode*> cont;
+        cont.push(root);
+        while(cont.size()){
+            queue<TreeNode*> tmp;
+            while(cont.size()){
+                root = cont.front();
+                cont.pop();
                 level.push_back(root->val);
-                if(root->left) cont2.push(root->left);
-                if(root->right) cont2.push(root->right);
-                if(!cont1.size()) {
-                    result.push_back(level);
-                    level.clear();
-                }
+                if(root->left) tmp.push(root->left);
+                if(root->right) tmp.push(root->right);
             }
-            while(cont2.size()){
-                root = cont2.front();
-                cont2.pop();
-                level.push_back(root->val);
-                if(root->left) cont1.push(root->left);
-                if(root->right) cont1.push(root->right);
-                if(!cont2.size()) {
-                    result.push_back(level);
-                    level.clear();
-                }
-            }
+            result.push_back(level);
+            level.clear();
+            cont=tmp;
         }
         
         return result;

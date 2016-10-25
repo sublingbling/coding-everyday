b176bdf5a66ac7f867ea84e13605f10da03e5659
xcviva <xcviva@gmail.com>
2016-10-24 23:45:15 -0700

    Create chang.java

---
+++ 10_24_2016_path_sum/chang.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return false;
+        }
+
+        if(root.left == null && root.right == null && root.val == sum) {
+            return true;
+        }
+        if(hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)) {
+            return true;
+        }
+        return false;
+    }
+}

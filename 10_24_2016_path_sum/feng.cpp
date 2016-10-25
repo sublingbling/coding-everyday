238e54ea2f76f63374980a7014fb4bb23ac55d12
tflchina <tflchina@gmail.com>
2016-10-24 22:17:56 -0700

    Create feng.cpp

---
+++ 10_24_2016_path_sum/feng.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root == NULL) return false;
+        sum -= root->val;
+        if((NULL==root->left) && (NULL==root->right)) return sum==0;
+        bool r=false;
+        if(root->left) r = r || hasPathSum(root->left,sum);
+        if(root->right) r = r || hasPathSum(root->right,sum);
+        return r;
+    }
+};
+

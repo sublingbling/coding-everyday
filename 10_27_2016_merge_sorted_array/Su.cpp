c8834256c1137a269afbfc9b2141e21b171e55ac
sublingbling <shopxiaohao@gmail.com>
2016-10-27 14:18:40 -0700

    Update Su.cpp

--- 10_27_2016_merge_sorted_array/Su.cpp
+++ 10_27_2016_merge_sorted_array/Su.cpp
@@ -1,11 +1,9 @@
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx=m+n;
-        
         while(n && m)
-            nums1[--idx] = (nums1[m-1]>nums2[n-1])?nums1[--m]:nums2[--n];
+            nums1[m+n-1] = (nums1[m-1]>nums2[n-1])?nums1[--m]:nums2[--n];
         
         while(n)
-            nums1[--idx] = nums2[--n];
+            nums1[m+n-1] = nums2[--n];
         
         return;
     }

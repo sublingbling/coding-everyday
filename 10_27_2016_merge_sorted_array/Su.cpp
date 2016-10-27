07f2ceba84b977e9eccbda1936f6ed90a5f86154
sublingbling <shopxiaohao@gmail.com>
2016-10-27 14:14:53 -0700

    Update Su.cpp

--- 10_27_2016_merge_sorted_array/Su.cpp
+++ 10_27_2016_merge_sorted_array/Su.cpp
@@ -4,7 +4,7 @@
         while(n && m)
             nums1[--idx] = (nums1[m-1]>nums2[n-1])?nums1[--m]:nums2[--n];
         
-        while(n && !m)
+        while(n)
             nums1[--idx] = nums2[--n];
         
         return;

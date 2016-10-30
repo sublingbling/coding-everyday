4701aee2708aa7f08a1dc2876bfc4ede008a191b
tflchina <tflchina@gmail.com>
2016-10-29 22:12:32 -0700

    Create feng.cpp

---
+++ 10-29-2016_best_time_to_buy_and_sell_stock/feng.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()<2) return 0;
+        int min_p, max_p, profit;
+        min_p= max_p= prices[0];
+        profit = 0;
+        for(auto a:prices){
+            if(a<=min_p) min_p = max_p = a;   //reset if small than previous smallest number;
+            else if(a>max_p){                 //update if larger than max_p
+            max_p = a;
+            profit = max(profit, max_p-min_p);    
+            }     
+        }
+        return profit;
+    }
+};

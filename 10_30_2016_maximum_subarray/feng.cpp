0f904edba8fcd00f2f6f7ac27d1d716236993703
tflchina <tflchina@gmail.com>
2016-10-30 22:46:36 -0700

    Update feng.cpp

--- 10_30_2016_maximum_subarray/feng.cpp
+++ 10_30_2016_maximum_subarray/feng.cpp
@@ -14,3 +14,20 @@ public:
         return max_acc;
     }
 };
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int max_acc, accum;
+        accum = 0;
+        max_acc = nums[0];
+        for(auto a:nums){
+            accum =a + (accum>0?accum:0); //if accum goes to negtive, start a new accum from 0
+            max_acc = max(accum,max_acc);     
+        }
+        return max_acc;
+    }
+};
+

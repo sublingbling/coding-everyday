1cc72b08f580f265de8adef666711c94bfef1d50
tflchina <tflchina@gmail.com>
2016-10-30 22:39:45 -0700

    Create feng.cpp

---
+++ 10_30_2016_maximum_subarray/feng.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        if(nums.size()==1) return nums[0];
+        int max_acc, accum;
+        accum = 0;
+        max_acc = nums[0];
+        for(auto a:nums){
+            accum +=a;
+            max_acc = max(accum,max_acc);
+            accum = max(0,accum);          //if accum goes to negtive, start a new accum from 0
+        }
+        return max_acc;
+    }
+};

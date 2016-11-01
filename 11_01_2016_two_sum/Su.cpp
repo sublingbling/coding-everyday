c0413fd2c9e0280ef93839d5a45ae3bbe0073a4a
sublingbling <shopxiaohao@gmail.com>
2016-11-01 14:55:29 -0700

    Update Su.cpp

--- 11_01_2016_two_sum/Su.cpp
+++ 11_01_2016_two_sum/Su.cpp
@@ -14,3 +14,29 @@ public:
         return idx;
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> idxp;
+        vector<int> idxn;
+        vector<int> *pidx=NULL;
+        int idx;
+        for(int i=0;i<nums.size();i++){
+            idx = target-nums[i];
+			pidx = (idx<0)?&idxn:&idxp;
+			if(abs(idx)<(*pidx).size() && (*pidx)[abs(idx)]) {
+				(*pidx).resize(2);
+				(*pidx)[0]=(*pidx)[abs(idx)]-1;
+				(*pidx)[1]=i;
+				return (*pidx);
+			}
+
+			idx = nums[i];
+            pidx = (idx<0)?&idxn:&idxp;
+            if(abs(idx)>=(*pidx).size()) (*pidx).resize(abs(idx)+1);
+			(*pidx)[abs(idx)]=i+1;
+        }
+		return (*pidx);
+    }
+};

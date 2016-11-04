80782f4438b32f3688aafb71922ccd1eca4cf214
tflchina <tflchina@gmail.com>
2016-11-03 21:55:12 -0700

    Create feng.cpp

---
+++ 11_04_2016_Invert_Binary_Tree/feng.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+   
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(NULL == root) return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* r = q.front();
+            q.pop();
+            TreeNode* t = r->left;
+            r->left = r->right;
+            r->right = t;
+            if(r->left) q.push(r->left);
+            if(r->right) q.push(r->right);
+        }
+        
+        return root;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+   
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(NULL == root) return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* r = q.front();
+            q.pop();
+            TreeNode* t = r->left;
+            r->left = r->right;
+            r->right = t;
+            if(r->left) q.push(r->left);
+            if(r->right) q.push(r->right);
+        }
+        
+        return root;
+    }
+};

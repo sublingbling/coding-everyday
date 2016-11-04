92298965a83c8d00f40b6097d20c6e27c5943656
sublingbling <shopxiaohao@gmail.com>
2016-11-04 15:44:20 -0700

    Update Su.cpp

--- 11_04_2016_Invert_Binary_Tree/Su.cpp
+++ 11_04_2016_Invert_Binary_Tree/Su.cpp
@@ -19,3 +19,30 @@ public:
         return root;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        stack<TreeNode*> nodes;
+        nodes.push(root);
+        while(nodes.size() && root){
+            TreeNode* roottmp=nodes.top();
+            nodes.pop();
+            TreeNode* tmp=roottmp->left;
+            roottmp->left=roottmp->right;
+            roottmp->right=tmp;
+            if(roottmp->left) nodes.push(roottmp->left);
+            if(roottmp->right) nodes.push(roottmp->right);
+        }
+        return root;
+    }
+};

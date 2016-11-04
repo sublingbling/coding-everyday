9a48ceb53f52f66067520de04883009901fd9c15
up824 <gin.1up@gmail.com>
2016-11-04 10:11:55 +0800

    Create up.py

---
+++ 11_03_2016_Binary_Tree_Path/up.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+        res, stack = [], [(root, "")]
+        
+        while stack:
+            node, ls = stack.pop()
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.right:
+                stack.append((node.right, ls+str(node.val)+"->"))
+            if node.left:
+                stack.append((node.left, ls+str(node.val)+"->"))
+                
+        return res

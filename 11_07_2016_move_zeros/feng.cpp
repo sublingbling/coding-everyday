397e275f586e6aeb18c022e5f096ae74a7f0e93d
tflchina <tflchina@gmail.com>
2016-11-07 01:04:06 -0800

    Create feng.cpp

---
+++ 11_07_2016_move_zeros/feng.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int idx=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]){
+                nums[idx]=nums[i]; 
+                idx++;
+            }
+        }
+        for(int i=idx; i<nums.size();i++) nums[i]=0;
+        return;
+    }
+};

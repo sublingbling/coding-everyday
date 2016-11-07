3c1abc50b638031ebf4257dbec70a2f0d353d73f
tflchina <tflchina@gmail.com>
2016-11-07 00:15:16 -0800

    add iterative solution

--- 11_06_2016_clone_graph/feng.cpp
+++ 11_06_2016_clone_graph/feng.cpp
@@ -19,3 +19,41 @@ public:
         return r;
     }
 };
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+private:
+    unordered_map<UndirectedGraphNode*,UndirectedGraphNode*> traveled;    
+public:
+    // use a queue to travel the graph
+    // use a map from old grpah to new graph, avoid duplicated copy.
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(node==NULL) return NULL;
+        queue<UndirectedGraphNode*> q;
+        q.push(node);
+        UndirectedGraphNode* new_node = new UndirectedGraphNode(node->label);
+        traveled.insert({node,new_node});
+        
+        while(!q.empty()){
+            UndirectedGraphNode* r = q.front();
+            q.pop();
+            for(auto a:r->neighbors){
+              if(traveled.find(a)==traveled.end()){  //new node
+                UndirectedGraphNode* new_a = new UndirectedGraphNode(a->label);
+                traveled[r]->neighbors.push_back(new_a);
+                traveled.insert({a,new_a});
+                q.push(a);
+              }
+              else traveled[r]->neighbors.push_back(traveled[a]); //traveled node
+            }
+        }
+        return new_node;
+    }
+};

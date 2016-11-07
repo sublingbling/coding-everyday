9046547fdef5b52953c781b4ffbbe4d5d3467567
xcviva <xcviva@gmail.com>
2016-11-07 00:01:01 -0800

    Create chang.py

---
+++ 11_06_2016_clone_graph/chang.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def __init__(self):
+        self.dict = {}
+        
+    def cloneGraph(self, node):
+        if node == None:
+            return None
+        if node.label in self.dict:
+            return self.dict[node.label]
+        root = UndirectedGraphNode(node.label)
+        self.dict[node.label] = root
+        for item in node.neighbors:
+            root.neighbors.append(self.cloneGraph(item))
+        return root

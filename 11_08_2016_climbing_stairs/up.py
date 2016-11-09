59621e998ed9e1170f0e8fb13f2e35694f472b54
up824 <gin.1up@gmail.com>
2016-11-08 17:32:00 -0800

    Update up.py

--- 11_08_2016_climbing_stairs/up.py
+++ 11_08_2016_climbing_stairs/up.py
@@ -5,11 +5,12 @@ class Solution(object):
         :rtype: int
         """
         
-        dp = [0 for x in range(n+1)]
+        if n < 2 :
+            return n
         
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        # now n>=2
+        dp = [1] * (n + 1)
+        for i in xrange(2,n + 1):
+            dp[i] = dp[i-2] + dp[i-1]
         
-        return dp[-1]
+        return dp[n]

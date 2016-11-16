c8d2276c30e30ce2d9e9972658195c9a12af94f3
qingsiw <qingsi@umich.edu>
2016-11-16 14:29:04 -0800

    add an earlier and involved solution

--- 11_16_2016-convert-sorted-array-to-binary-search-tree/qingsi.cpp
+++ 11_16_2016-convert-sorted-array-to-binary-search-tree/qingsi.cpp
@@ -25,3 +25,46 @@ public:
         return _sortedArrayToBST(nums, 0, nums.size());
     }
 };
+
+class SolutionAlternative {
+    int _idx = 0;
+    vector<int> _nums;
+    TreeNode* buildTree(int numNodes) {
+        if (!numNodes)
+            return NULL;
+        if (numNodes == 1) {
+            auto root = new TreeNode(-1);
+            return root;
+        }
+        if (numNodes == 2) {
+            auto root = new TreeNode(-1);
+            auto leaf = new TreeNode(-1);
+            root->left = leaf;
+            return root;
+        }
+        auto root = new TreeNode(-1);
+        auto leftChild = buildTree(numNodes/2);
+        auto rightChild = buildTree(numNodes - 1 - numNodes/2);
+        root->left = leftChild;
+        root->right = rightChild;
+        return root;
+    }
+    void labelTree(TreeNode* root) {
+        if (!root)
+            return;
+        if (root->left)
+            labelTree(root->left);
+        root->val = _nums[_idx++];
+        if (root->right)
+            labelTree(root->right);
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        auto root = buildTree(nums.size());
+        if (!root)
+            return root;
+        _nums.assign(nums.begin(), nums.end());
+        labelTree(root);
+        return root;
+    }
+};

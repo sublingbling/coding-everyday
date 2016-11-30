bdda33c44d65ffa77a44039a3539c4dec0c251a0
tflchina <tflchina@gmail.com>
2016-11-30 01:41:16 -0800

    optimized code to avoid insert when node is larger than tail

--- 2016-11-29-insertion-sort-list/feng.cpp
+++ 2016-11-29-insertion-sort-list/feng.cpp
@@ -32,3 +32,47 @@ public:
         return dummy_head->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* tail;
+    void insert(ListNode* to, ListNode* from){
+        while(to){
+            if(to->next == NULL) tail = from;
+            if(to->next == NULL||to->next->val>from->val){
+                from->next = to->next;
+                to->next = from;
+                return;
+            }
+            
+            to = to->next;
+        }
+    }
+   
+    
+public:
+    ListNode* insertionSortList(ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* dummy_head = new ListNode(0);
+        tail=NULL;
+        while(head){
+            ListNode* t = head->next;
+            if(tail && head->val>tail->val){ 
+                tail->next = head;
+                tail = tail->next;
+                head->next = NULL;
+            }
+            else insert(dummy_head, head);
+            head = t;
+        }
+        return dummy_head->next;
+    }
+};

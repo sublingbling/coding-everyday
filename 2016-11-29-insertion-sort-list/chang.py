5a67ad85b0e7fffe597ea1f191c83767d5760560
xcviva <xcviva@gmail.com>
2016-11-30 21:18:02 -0800

    Update chang.py

--- 2016-11-29-insertion-sort-list/chang.py
+++ 2016-11-29-insertion-sort-list/chang.py
@@ -1,20 +1,39 @@
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
 class Solution:
     """
     @param head: The first node of linked list.
     @return: The head of linked list.
     """ 
     def insertionSortList(self, head):
-        # write your code here
         dummy = ListNode(0)
-
-        while head:
-            temp = dummy
-            next = head.next
-            while temp.next and temp.next.val < head.val:
-                temp = temp.next
-
-            head.next = temp.next
-            temp.next = head
-            head = next
         
+        #dummy.next = head
+        cur = head #current node to be inserted
+        pre = dummy #insert between pre and pre.next
+        next = None #next node to be inserted
+        
+        while cur is not None:
+            pre = dummy
+            next = cur.next
+            #find the node to insert after it
+            while pre.next is not None and pre.next.val < cur.val:
+                pre = pre.next
+            
+            cur.next = pre.next
+            pre.next = cur
+
+            cur = next
+            
         return dummy.next
+            
+            
+        
+        
+        

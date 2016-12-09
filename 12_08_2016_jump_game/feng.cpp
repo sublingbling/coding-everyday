d57022d174c8bccdf741a8892ace62abcfd10ca6
tflchina <tflchina@gmail.com>
2016-12-09 00:40:08 -0800

    Create feng.cpp

---
+++ 12_08_2016_jump_game/feng.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()<2) return true;
+        int max_idx = 0;
+        int idx = 0;
+        while(max_idx<nums.size()-1){
+            max_idx = max(max_idx,idx+nums[idx]);
+            if(idx == max_idx && nums[idx]==0) return false;
+            idx++;
+        }
+        return true;
+    }
+};

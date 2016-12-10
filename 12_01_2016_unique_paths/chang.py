238c5d28503f2494adff2f71882808187d651ce2
xcviva <xcviva@gmail.com>
2016-12-09 22:00:07 -0800

    Update chang.py

--- 12_01_2016_unique_paths/chang.py
+++ 12_01_2016_unique_paths/chang.py
@@ -1,14 +1,21 @@
 class Solution:
-    # @return an integer
-    def c(self, m, n):
-        mp = {}
-        for i in range(m):
-            for j in range(n):
-                if(i == 0 or j == 0):
-                    mp[(i, j)] = 1
-                else:
-                    mp[(i, j)] = mp[(i - 1, j)] + mp[(i, j - 1)]
-        return mp[(m - 1, n - 1)]
-
+    """
+    @param n and m: positive integer(1 <= n , m <= 100)
+    @return an integer
+    """ 
     def uniquePaths(self, m, n):
-        return self.c(m, n)
+        if m == 0 or n == 0:
+            return 0
+            
+        A = [[1 for x in range(n)] for x in range(m)]
+            
+        '''for i in range(0, m):
+            A[i][0] = 1
+        for i in range(0, n):
+            A[0][i] = 1'''
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                A[i][j] = A[i-1][j] + A[i][j-1]
+                
+        return A[-1][-1]

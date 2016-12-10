7351cf3c958e4d229034cd9e01db25a21d2c4dad
xcviva <xcviva@gmail.com>
2016-12-10 14:19:12 -0800

    Update chang.py

--- 12_02_2016_unique_paths_ii/chang.py
+++ 12_02_2016_unique_paths_ii/chang.py
@@ -1,27 +1,32 @@
 class Solution:
-
+    """
+    @param obstacleGrid: An list of lists of integers
+    @return: An integer
+    """
     def uniquePathsWithObstacles(self, obstacleGrid):
-        mp = obstacleGrid
-        for i in range(len(mp)):
-            for j in range(len(mp[i])):
+        if obstacleGrid is None or len(obstacleGrid) == 0 or len(obstacleGrid[0]) == 0:
+            return 0
+            
+        A = obstacleGrid
+        
+        for i in range(len(A)):
+            for j in range(len(A[0])):
                 if i == 0 and j == 0:
-                    mp[i][j] = 1 - mp[i][j]
+                    A[i][j] = 1 - A[i][j]
                 elif i == 0:
-                    if mp[i][j] == 1:
-                        mp[i][j] = 0
+                    if A[i][j] == 1:
+                        A[i][j] = 0
                     else:
-                        mp[i][j] = mp[i][j - 1]
+                        A[i][j] = A[i][j-1]
                 elif j == 0:
-                    if mp[i][j] == 1:
-                        mp[i][j] = 0
+                    if A[i][j] == 1:
+                        A[i][j] = 0
                     else:
-                        mp[i][j] = mp[i - 1][j]
+                        A[i][j] = A[i-1][j]
                 else:
-                    if mp[i][j] == 1:
-                        mp[i][j] = 0
+                    if A[i][j] == 1:
+                        A[i][j] = 0
                     else:
-                        mp[i][j] = mp[i - 1][j] + mp[i][j - 1]
-        if mp[-1][-1] > 2147483647: 
-            return -1
-        else:
-            return mp[-1][-1]
+                        A[i][j] = A[i-1][j] + A[i][j-1]
+        
+        return A[-1][-1]

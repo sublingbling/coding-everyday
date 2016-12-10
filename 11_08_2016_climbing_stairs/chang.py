20ec9cf57b6da2b22ceeaad93caf68f473f81f44
xcviva <xcviva@gmail.com>
2016-12-10 14:49:35 -0800

    Update chang.py

--- 11_08_2016_climbing_stairs/chang.py
+++ 11_08_2016_climbing_stairs/chang.py
@@ -1,13 +1,15 @@
 class Solution:
     """
-    @param n: a integer
-    @return: a integer
+    @param n: An integer
+    @return: An integer
     """
     def climbStairs(self, n):
-        # write your code here
-        if n <= 2:
-            return n
-        result=[1,2]
-        for i in range(n-2):
-            result.append(result[-2]+result[-1])
-        return result[-1]
+        if n < 2:
+            return 1
+            
+        fb = [1 for x in range(n+1)]
+        
+        for i in range(2,n+1):
+            fb[i] = fb[i-1] + fb[i-2]
+        
+        return fb[n]

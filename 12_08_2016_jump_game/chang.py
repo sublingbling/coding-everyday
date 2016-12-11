33376478ff3865a85cb90badc9081a9518cf9397
xcviva <xcviva@gmail.com>
2016-12-10 21:14:39 -0800

    Update chang.py

--- 12_08_2016_jump_game/chang.py
+++ 12_08_2016_jump_game/chang.py
@@ -1,12 +1,22 @@
+#DP method
 class Solution:
     # @param A, a list of integers
     # @return a boolean
     def canJump(self, A):
-        p = 0
-        ans = 0
-        for item in A[:-1]:
-            ans = max(ans, p + item)
-            if(ans <= p):
-                return False
-            p += 1
-        return True
+        if A is None or len(A) == 0:
+            return False
+        
+        DP = [False] * (len(A))
+        DP[0] = True
+        
+        for i in xrange(1,len(A)):
+            for j in xrange(i):
+                if DP[j] is True and A[j] + j >= i:
+                    DP[i] = True
+                    break
+                
+        return DP[-1]
+    
+#Greedy
+    
+    

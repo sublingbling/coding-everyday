90c782c34e55a7b6dd363a5a5238d5ba5cbf8893
tflchina <tflchina@gmail.com>
2016-12-11 23:13:36 -0800

    Create feng.cpp

---
+++ 12_11_2016_longest_increasing_subsequence/feng.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        vector<int> r;
+        for(auto a:nums){
+            if(r.empty()||a>r.back()) r.push_back(a);
+            else{
+                for(auto& b:r) {  //since r is increase order, can use binary search for further optimize
+                    if(a<=b){
+                        b=a;
+                        break;
+                    }
+                }
+            }
+        }
+        return r.size();
+    }
+};

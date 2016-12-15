9f7449d25b279530107101eb9be65a8c5492078a
tflchina <tflchina@gmail.com>
2016-12-14 22:40:03 -0800

    //optimize to eliminate unecessary copy

--- 12_13_2016_palindrome_partitioning/feng.cpp
+++ 12_13_2016_palindrome_partitioning/feng.cpp
@@ -42,3 +42,44 @@ public:
         return partition(rev_s,0);
     }
 };
+
+
+//optimize to eliminate unecessary copy
+
+class Solution {
+private:
+    vector<vector<int>> a2b;
+    void partition(string& s, vector<string>& pt, vector<vector<string>>& p, int i){
+        if(i>=s.size()){
+            p.push_back(pt);
+            return;
+        } 
+        for(int j=0; j<a2b[i].size();j++){
+            int r= a2b[i][j];
+            pt.push_back(s.substr(i,r-i+1));
+            partition(s,pt, p, r+1);
+            pt.pop_back();
+        }
+    }
+public:
+    vector<vector<string>> partition(string s){
+        //build palindrome map, a2b[l] store all end idx of palindrome start from l
+        vector<int> t;
+        for(int i=0; i<s.size();i++) a2b.push_back(t);
+        for(int i=0; i<2*s.size(); i++){
+            int l = i>>1;
+            int r = (i+1)>>1;
+            while(l>=0 && r< s.size()&& s[l]==s[r]){
+                a2b[l].push_back(r);   //l,r are start and end idx of a palindrome
+                l--;
+                r++;
+            }
+        }
+        
+        //partition
+        vector<string> pt;
+        vector<vector<string>> p;
+        partition(s, pt, p, 0);
+        return p;
+    }
+};

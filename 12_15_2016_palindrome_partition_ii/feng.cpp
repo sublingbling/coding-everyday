98665dc8288aaf4c2c4737d93ab1c2db6009e2a8
tflchina <tflchina@gmail.com>
2016-12-16 11:16:26 -0800

    combine map build and step count

--- 12_15_2016_palindrome_partition_ii/feng.cpp
+++ 12_15_2016_palindrome_partition_ii/feng.cpp
@@ -32,3 +32,28 @@ public:
         return min(steps_l[s.size()-1],steps_r[s.size()-1]);
     }
 };
+
+//combine map build and step count
+class Solution {
+public:
+    int minCut(string s) {
+        //how many steps it take from 0 to size()-1;
+        int* steps_l = new int[s.size()+1];
+        int* steps_r = new int[s.size()+1];
+        steps_l[0] = steps_r[0] = 0;
+        for(int i =1; i<s.size(); i++) steps_l[i] = steps_r[i] = INT_MAX;
+        
+        for(int i=0; i<2*s.size(); i++){
+            int l = i>>1;
+            int r = (i+1)>>1;
+            while(l>=0 && r< s.size()&& s[l]==s[r]){
+                steps_l[r+1] = min(steps_l[r+1], steps_l[l]+1);
+                steps_r[r] = min(steps_l[l],steps_r[r]);
+                l--;
+                r++;
+            }
+        }
+        
+        return min(steps_l[s.size()-1],steps_r[s.size()-1]);
+    }
+};

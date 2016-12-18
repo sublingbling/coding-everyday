0e3d88d8e58b910d78abc9a9b797533fac0f0ff6
xcviva <xcviva@gmail.com>
2016-12-17 23:58:54 -0800

    Update chang.py

--- 12_15_2016_palindrome_partition_ii/chang.py
+++ 12_15_2016_palindrome_partition_ii/chang.py
@@ -2,15 +2,34 @@ class Solution:
     # @param s, a string
     # @return an integer
     def minCut(self, s):
-        n = len(s)
-        f = []
-        p = [[False for x in range(n)] for x in range(n)]
-        #the worst case is cutting by each char
-        for i in range(n+1):
-            f.append(n - 1 - i) # the last one, f[n]=-1
-        for i in reversed(range(n)):
-            for j in range(i, n):
-                if (s[i] == s[j] and (j - i < 2 or p[i + 1][j - 1])):
-                    p[i][j] = True
-                    f[i] = min(f[i], f[j + 1] + 1)
-        return f[0]
+        if not s or len(s) == 0:
+            return 0
+        isPar = self.isPalindrome(s)
+        
+        dp = []
+        for i in xrange(len(s) + 1):
+            dp.append(i - 1)
+            
+        for i in xrange(1, len(s) + 1):
+            for j in xrange(i):
+                if isPar[j][i - 1]:
+                    dp[i] = min(dp[i], dp[j] + 1)
+                    
+        return dp[len(s)]
+        
+    def isPalindrome(self, s):
+        slen = len(s)
+        isPal = [[False for x in range(slen)] for y in range(slen)]
+        
+        for i in xrange(slen):
+            isPal[i][i] = True
+            
+        for i in xrange(slen - 1):
+            isPal[i][i + 1] = (s[i] == s[i + 1])
+            
+        for length in xrange(2, slen):
+            for start in xrange(slen - length):
+                isPal[start][start + length] = isPal[start + 1][start + length -1] and (s[start] == s[start + length])
+                
+        return isPal
+            
